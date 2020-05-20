c53ae34ab4e5a18a07bcd88459ce95bd1fd07ca2
Andy <wilson.stats@gmail.com>
2020-05-20 09:04:21 -0600

    Update map with positivity layer

--- Codes/RCode.R
+++ Codes/RCode.R
@@ -20,15 +20,19 @@ lat_max = 49
 lon_max = -116.85 
 lon_min = -124.75
 
-## Import prevalence data 
-
+## Import positivity data 
 
 prev <- read.csv(file = "External data/ZIP3_XY_HPV_SCREEN_2019_SLIM.csv", na.strings = c("", "NA"))
+
+## Restrict to Washington state
 Wash_prev <- prev %>%
-  filter(Longitude_avg >=  -124.46) %>%
-  filter(Longitude_avg <=  -116.55) %>%
-  filter(Latitude_avg  >=  45.33) %>%
-  filter(Latitude_avg  <=  49)
+  filter(Longitude_avg >=  lon_min) %>%
+  filter(Longitude_avg <=  lon_max) %>%
+  filter(Latitude_avg  >=  lat_min) %>%
+  filter(Latitude_avg  <=  lat_max) %>%
+  filter(zip3 != "971")
+
+summary(Wash_prev)
 
 #get the list of states from us_state_polygons.json
 states <- geojsonio::geojson_read("us_state_polygons.json", what = "sp")
@@ -130,39 +134,7 @@ m <- leaflet(mydf, options = leafletOptions(dragging=TRUE,
 pal  <- colorNumeric(palette = "RdYlBu", domain =c(0:45))
 pal2 <- colorNumeric(palette = "RdYlBu", domain =c(0:140), reverse = TRUE)
 pal3 <- colorNumeric(palette = "RdYlBu", domain =c(0:15), reverse = TRUE)
-
-
-
-MM <- m %>% addPolygons(data = mydf, weight=1, fillOpacity = 0.75,
-                        color = pal(mydf$All),
-                        label = mydf$NAME,
-                        popup = paste0("Percent change: ", 
-                                       mydf$All, "%"),
-                        labelOptions = labelOptions(textOnly = FALSE),
-                        group = "Factor") %>%
-  addCircleMarkers(lng = myWashIRS$longitude,
-                   lat = myWashIRS$latitude,
-                   label = myWashIRS$PlaceName,
-                   popup = paste("<b>", myWashIRS$PlaceName,"</b>",
-                                 "<br/>",
-                                 " Address = ", myWashIRS$Address,
-                                 "<br/>",
-                                 "ID = ", myWashIRS$PlaceName),
-                   radius = myWashIRS$AdjInc,
-                   color = pal2(myWashIRS$AdjInc), 
-                   group = "Sites",
-                   clusterOptions = markerClusterOptions())%>% 
-  addLayersControl(overlayGroups = c("Factor", "Sites"))
-
-MMM <- MM %>% addResetMapButton() %>%
-  addLegend(title = "Percent case change (1 day)", 
-            position = "bottomleft",
-            pal=pal,
-            values = c(0:45))
-MMM
-saveWidget(MMM, file = "C:\\Users\\wilso\\Documents\\GitHub\\COVID-19-strategic-feasibility-support\\Outputs\\New York example.html")
-
-pal3 <- colorNumeric(palette = "RdYlBu", domain =c(0:15), reverse = TRUE)
+pal4 <- colorNumeric(palette = "Blues", domain =c(1:50))
 
 
 MM <- m %>% addPolygons(data = mydf, weight=1, fillOpacity = 0.45,
@@ -188,12 +160,26 @@ MM <- m %>% addPolygons(data = mydf, weight=1, fillOpacity = 0.45,
                                  "<br/>",
                                  "zip = ", myWashIRS$zip),
                    radius = myWashIRS$AdjInc,
-                   stroke = FALSE, fillOpacity = 0.5,
+                   stroke = TRUE, fillOpacity = 0.5,
                    color = pal2(myWashIRS$AdjInc), 
                    group = "IRS tax data by zip",
-                   clusterOptions = markerClusterOptions())%>% 
-  
-  addLayersControl(overlayGroups = c("County HPV vaccination", "IRS tax data by zip", "County Uninsured"))
+                   clusterOptions = markerClusterOptions()) %>% 
+  addCircleMarkers(lng = Wash_prev$Longitude_avg,
+                   lat = Wash_prev$Latitude_avg,
+                   label = Wash_prev$zip3,
+                   popup = paste("<b>", Wash_prev$zip3,"</b>",
+                                 "<br/>",
+                                 " Positivity rate: ", round(Wash_prev$positivity,2), "%", 
+                                 "<br/>",
+                                 "n screened = ", Wash_prev$n_screened,
+                                 "<br/>",
+                                 "n positive = ", Wash_prev$n_positive
+                                 ),
+                   radius = log(Wash_prev$n_positive),
+                   stroke = TRUE, fillOpacity = 0.8,
+                   color = pal4(Wash_prev$positivity), 
+                   group = "Positivity by 3-digit zip") %>%
+  addLayersControl(overlayGroups = c("County HPV vaccination", "IRS tax data by zip", "County Uninsured", "Positivity by 3-digit zip"))
 
 MMM <- MM %>% addResetMapButton() %>%
   addLegend(title = "Vaccination rates", 
@@ -207,9 +193,44 @@ MMM <- MM %>% addResetMapButton() %>%
   addLegend(title = "Percent uninsured", 
             position = "topleft",
             pal=pal3,
-            values = c(0:15))
+            values = c(0:15)) %>%
+  addLegend(title = "Positivity rate", 
+            position = "topright",
+            pal=pal4,
+            values = c(0:50))
 MMM
 
-saveWidget(MMM, file = "/Users/andywilson1/Documents/GitHub/Olivia-DIA/Output/Prototype Washington map v0.1.html")
+#MAC AW
+#saveWidget(MMM, file = "/Users/andywilson1/Documents/GitHub/Olivia-DIA/Output/Prototype Washington map v0.1.html")
 
+#PC AW
+saveWidget(MMM, file = "C:/Users/wilso/Documents/GitHub/Olivia-DIA/Output/Prototype Washington map v1.0.html")
 
+# MM <- m %>% addPolygons(data = mydf, weight=1, fillOpacity = 0.75,
+#                         color = pal(mydf$All),
+#                         label = mydf$NAME,
+#                         popup = paste0("Percent change: ", 
+#                                        mydf$All, "%"),
+#                         labelOptions = labelOptions(textOnly = FALSE),
+#                         group = "Factor") %>%
+#   addCircleMarkers(lng = myWashIRS$longitude,
+#                    lat = myWashIRS$latitude,
+#                    label = myWashIRS$PlaceName,
+#                    popup = paste("<b>", myWashIRS$PlaceName,"</b>",
+#                                  "<br/>",
+#                                  " Address = ", myWashIRS$Address,
+#                                  "<br/>",
+#                                  "ID = ", myWashIRS$PlaceName),
+#                    radius = myWashIRS$AdjInc,
+#                    color = pal2(myWashIRS$AdjInc), 
+#                    group = "Sites",
+#                    clusterOptions = markerClusterOptions())%>% 
+#   addLayersControl(overlayGroups = c("Factor", "Sites"))
+# 
+# MMM <- MM %>% addResetMapButton() %>%
+#   addLegend(title = "Percent case change (1 day)", 
+#             position = "bottomleft",
+#             pal=pal,
+#             values = c(0:45))
+# MMM
+# saveWidget(MMM, file = "C:\\Users\\wilso\\Documents\\GitHub\\COVID-19-strategic-feasibility-support\\Outputs\\New York example.html")
